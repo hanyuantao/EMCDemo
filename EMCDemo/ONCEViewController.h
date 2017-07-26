41e4f040c0456bfe7ce40f287263b116965aa852
once <545574484@qq.com>
2017-07-26 15:49:03 +0800

    1.1.1

---
+++ EMCDemo/ONCEViewController.h
@@ -0,0 +1,13 @@
+//
+//  ONCEViewController.h
+//  EMC
+//
+//  Created by 孔辉 on 2017/7/26.
+//  Copyright © 2017年 school. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ONCEViewController : UIViewController
+
+@end
---
+++ EMCDemo/ONCEViewController.m
@@ -0,0 +1,36 @@
+//
+//  ONCEViewController.m
+//  EMC
+//
+//  Created by 孔辉 on 2017/7/26.
+//  Copyright © 2017年 school. All rights reserved.
+//
+
+#import "ONCEViewController.h"
+
+@interface ONCEViewController ()
+
+@end
+
+@implementation ONCEViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
